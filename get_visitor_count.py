15d6d3e11b88b0402fa815da10e5fd6e09faa134
cjuliachen <juliachencyre@gmail.com>
2023-02-28 11:05:34 -0500

    fix lint issues

--- get_visitor_count.py
+++ get_visitor_count.py
@@ -14,30 +14,30 @@ def get_visitor_count(event, context):
     """
     client = boto3.client('dynamodb')
     response = client.update_item(
-        TableName = d_db_name,
-        Key = {
-            'id':{
+        TableName=d_db_name,
+        Key={
+            'id': {
                 'S': 'visitor'}
         },
-        UpdateExpression= "SET hits = hits + :incr",
-        ExpressionAttributeValues = {
+        UpdateExpression="SET hits = hits + :incr",
+        ExpressionAttributeValues={
             ':incr': {'N': '1'}
         },
         ReturnValues="UPDATED_NEW",
     )
     response_message = {
-    'statusCode': 200,
-    'headers': {
-        "Content-Type": "application/json",
-        'Access-Control-Allow-Origin': 'https://'+ domain,
-        'Access-Control-Allow-Methods': 'OPTION, POST',
-        'Access-Control-Allow-Headers': 'Content-Type',
-    },
-    "body": json.dumps({
-        "message": "success",
-        "count": response['Attributes']['hits']['N']
+        'statusCode': 200,
+        'headers': {
+            "Content-Type": "application/json",
+            'Access-Control-Allow-Origin': "https://"+ domain,
+            'Access-Control-Allow-Methods': "OPTION, POST",
+            'Access-Control-Allow-Headers': "Content-Type",
+        },
+        "body": json.dumps({
+            "message": "success",
+            "count": response['Attributes']['hits']['N']
     }),
-
 }
     return response_message
-    
+
+
